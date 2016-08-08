1754c938e295aa88651b6ee2d9461c31fe14c800
John Rodwell <john.rodwell@draysontechnologies.com>
2016-08-08 02:36:23 +0100

    Ready for WPEngine

--- wp-content/themes/sensyne/footer.php
+++ wp-content/themes/sensyne/footer.php
@@ -15,7 +15,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'sensyne' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'sensyne' ), 'WordPress' ); ?></a>
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'sensyne' ) ); ?>"><?php printf( esc_html__( 'Powered by %s', 'sensyne' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'sensyne' ), 'sensyne', '<a href="http://www.bashthekeyboard.co.uk/" rel="designer">John Rodwell</a>' ); ?>
 		</div><!-- .site-info -->
